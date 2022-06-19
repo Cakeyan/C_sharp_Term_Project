c3d85f828c5748f8be2a0dd09054f2aef4271948
chenwei746 <1946146453@qq.com>
2022-06-19 19:09:31 +0800

    完善代码逻辑并添加注释

--- Server/Server/CC.cs
+++ Server/Server/CC.cs
@@ -8,11 +8,12 @@ using System.Timers;
 
 namespace Server
 {
+    // 用来存储当前在线的用户类
     public class CC
     {
         public static List<User> LoginUsers { get; set; }
         public static List<MyUser> Users { get; set; }
-        //public static Dictionary<string, MyUser> Users = new Dictionary<string, MyUser>();
+
         public static Dictionary<int, Room> Rooms = new Dictionary<int, Room>();
         public static MyUser GetUser(string username)
         {
@@ -29,6 +30,7 @@ namespace Server
         }
     }
 
+    // 游戏计时的时钟类
     public class MyTimer: Timer
     {
         public int restTime { get; set; }
