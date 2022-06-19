a654e92590184b8808ec06575b6402eeddde7585
msm8976 <msm8976@qq.com>
2022-06-19 21:03:25 +0800

    Revert "Revert "客户端添加部分注释""
    This reverts commit 042fb7fc6a4d22f80add12b229a85cdf5bdb045c.

--- Client/Client/ForgetPwWindow.xaml.cs
+++ Client/Client/ForgetPwWindow.xaml.cs
@@ -1,32 +1,18 @@
-﻿using Client.ServiceReference;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Drawing;
-using System.Linq;
-using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Client.LoginReference;
 
 namespace Client
 {
-    /// <summary>
-    /// ForgetPasswordWindow.xaml 的交互逻辑
-    /// </summary>
+    //找回密码逻辑
     public partial class ForgetPwWindow : Window
     {
-        
-        //验证码
         private string Verification;
         private LoginServiceClient client;
+
         public ForgetPwWindow()
         {
             client = new LoginServiceClient();
@@ -34,25 +20,25 @@ namespace Client
             Verification = GetImage();
         }
 
-        //以字符串返回验证码
+        //生成验证码图片
         public string GetImage()
         {
             string code = "";
             Bitmap bitmap = VerifyCodeHelper.CreateVerifyCode(out code);
             ImageSource imageSource = ImageFormatConvertHelper.ChangeBitmapToImageSource(bitmap);
             img.Source = imageSource;
-            //为了实现不区分大小写
             code = code.ToLower();
             return code;
         }
 
-        //Closing事件
+        //关闭找回窗口
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             LoginWindow LW = new LoginWindow();
             LW.Show();
         }
 
+        //回车提交
         private void Forget_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -61,16 +47,11 @@ namespace Client
             }
         }
 
-
-        //点击按钮事件
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
+        //发送令牌
         private void Button_Click_SendCode(object sender, RoutedEventArgs e)
         {
 
-            //判断是否为机器，验证码的真伪
+            //判断验证码
             if (Code.Text.ToLower() != Verification)
             {
                 MessageBox.Show("验证码输入错误！请重新输入", "提示", MessageBoxButton.OKCancel, MessageBoxImage.Asterisk);
@@ -78,7 +59,7 @@ namespace Client
                 Code.Text = "";
                 return;
             }
-
+            //发送令牌邮件
             try
             {
                 bool flag = client.sendEmail(Account.Text);
@@ -93,9 +74,9 @@ namespace Client
             }
         }
 
+        //修改密码
         private void Button_Click_ResetPassword(object sender, RoutedEventArgs e)
         {
-            //开始向服务端申请，尝试修改密码
             try
             {
                 string flag = client.ForgetPassword(Account.Text, PassWord.Password,mailCode.Text);
@@ -115,11 +96,13 @@ namespace Client
             }
         }
 
+        //点击返回按钮
         private void Button_Click_Back(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
+        //点击图片切换验证码
         private void img_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Verification = GetImage();
--- Client/Client/LoginWindow.xaml.cs
+++ Client/Client/LoginWindow.xaml.cs
@@ -1,31 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.ServiceModel;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using Client.ServiceReference;
 using Client.LoginReference;
 
 namespace Client
 {
-    /// <summary>
-    /// Login.xaml 的交互逻辑
-    /// </summary>
+    //登录界面逻辑
     public partial class LoginWindow : Window
     {
-        //全局变量
-        private User item;//每一个id所属，item可以控制该id下的所有窗口
-        private LoginServiceClient client;//服务端接口
-        private LoginReference.User us;//该用户的所有信息
+        private User item;
+        private LoginServiceClient client;
+        private LoginReference.User us;
+
         public LoginWindow()
         {
             InitializeComponent();
@@ -33,6 +20,7 @@ namespace Client
             WindowStartupLocation = WindowStartupLocation.CenterScreen;
         }
         
+        //回车登录
         private void Login_KeyUp(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
@@ -41,32 +29,34 @@ namespace Client
             }
         }
 
-
-        //所有button事件
+        //点击按钮事件
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (e.Source == forgetPw)//忘记密码事件
+            //找回
+            if (e.Source == forgetPw)
             {
                 this.Close();
                 ForgetPwWindow FP = new ForgetPwWindow();
                 FP.Show();
             }
-            else if (e.Source == sign_for)//注册事件
+            //注册
+            else if (e.Source == sign_for)
             {
                 this.Close();
                 RegisteredWindow RW = new RegisteredWindow();
                 RW.Show();
-            }else
+            }
+            //登录
+            else
             {
                 try
                 {
-                    //登录检测，true则为登录成功
+                    //登录判断
                     bool flag = client.Login(account.Text, passward.Password);
                     if (flag)
                     {
-                        //登录成功首先获取到该用户的所有信息，然后为传参做准备
+                        //获取登录用户信息
                         us = client.Userinfo(account.Text);
-                        //生成该用户的关联窗体的关系
                         if (CC.Users == null)
                         {
                             CC.Users = new List<User>();
@@ -76,9 +66,8 @@ namespace Client
                         item = CC.GetUser(us.Acount);
                         item.LoginWindow = this;
                         item.LoginWindow.Close();
-                       // item.MainWindow.ShowLogin(us.Name);
 
-                        //再显示登录后的界面，room
+                        //显示大厅界面
                         RoomWindow RW = new RoomWindow(us);
                         item.RoomWindow = RW;
                         item.RoomWindow.Show();
@@ -92,14 +81,7 @@ namespace Client
                 {
                     MessageBox.Show("未连接到服务器！");
                 }
-                
             }
-            
-        }
-
-        private void sign_in_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
